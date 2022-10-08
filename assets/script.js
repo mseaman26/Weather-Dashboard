bd5fade43cf1c02accb3c8ddaa7236fcad2052b0
michael seaman <mseaman26@gmail.com>
2022-10-08 09:40:07 -0700

    got some data from both the geo api and weather api by having all the code within the same function.  Couldn't seem to pass data from one fetch-related function to another function

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
-
+    var CurrentWeatherMapURL = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}"
+    var GeoCodingURL = "http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={lon}&limit={limit}&appid={API key}"
     var apiKay = "76e3a7084f08e874515947d6a36a78d5"
     var cityListDiv = document.getElementById("city-list-div")
-
+    var city = "London"
     function displayCities(){
         for(var i = 0;i<5;i++){
             var city = document.createElement ("div")
@@ -10,24 +11,41 @@ $(document).ready(function(){
             cityListDiv.appendChild(city)
         }
     }
-
+   
+    //var latAndLon = getLatandLon(city)
 
     function getWeatherData(city){
         console.log("hello")
-        fetch("http://api.openweathermap.org/geo/1.0/direct?q="+city+"&limit=5&appid=76e3a7084f08e874515947d6a36a78d5")
+        fetch("http://api.openweathermap.org/geo/1.0/direct?q="+city+"&limit=5&appid="+apiKay)
             .then(function (response) {
-                
+                //console.log(response.json())
                 return response.json()
             })
             .then(function(data){
                 var lat = data[0].lat  
                 var lon = data[0].lon
                 console.log([lat, lon])
+                fetch("https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid="+apiKay)
+                console.log(data[0].name)
             })
+        
     }
+    // function getWeatherData(city){
+    //     var latAndLon = getLatandLon(city)
+    //     console.log(latAndLon)
+    //     fetch("https://api.openweathermap.org/data/2.5/weather?lat="+latAndLon[0]+"&"+latAndLon[1]+"={lon}&appid=76e3a7084f08e874515947d6a36a78d5")
+    //     .then(function(response){
+    //         return response.json()
+    //     })
+    //     .then(function(data){
+    //         console.log(data)
+    //     })
+    // }
+
 //===================================Event Listener==================================================
 document.getElementById("sumbit-button").addEventListener("click", function(event){
-    getWeatherData("London")
+    event.preventDefault()
+    getWeatherData(city)
 })
 
 
@@ -35,3 +53,5 @@ document.getElementById("sumbit-button").addEventListener("click", function(even
 //=====================================================================================
 displayCities()
 })
+
+   
